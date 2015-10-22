adf8d49679c734266a299cd388a21a4384f91d69
Umesh <rsu1991@gmail.com>
2015-10-22 19:25:32 +0530

    Added a login button which gets the authToken using the REST API and sends that login token to the chat server to force the iframe to display the logged in screen.

--- client/views/rocket_chat/rocketChat.js
+++ client/views/rocket_chat/rocketChat.js
@@ -1,3 +1,5 @@
+Session.set("src", "");
+
 Template.rocketChat.helpers({
   version: function () {
     Meteor.call("getVersion", function (error, result) {
@@ -5,5 +7,36 @@ Template.rocketChat.helpers({
     });
 
     return Session.get("version");
+  },
+  src: function () {
+    return Session.get("src");
+  },
+  style: function () {
+    if (Session.get("src") !== "") {
+      return "display: block;";
+    }
+    return "display: none;";
+  }
+});
+
+Template.rocketChat.events({
+  'click #login-btn': function (event) {
+    console.log("Login button");
+
+    HTTP.post("http://192.168.1.7:4000/api/login",
+      {
+        data: {
+          password: "umesh",
+          user: "umesh"
+        }
+      }, function (error, result) {
+        if (error) {
+          console.log(error);
+        }
+        console.log(result);
+        var url = "http://192.168.1.7:4000?token=" + result.data.data.authToken;
+        console.log(url);
+        Session.set("src", url);
+    });
   }
 });
