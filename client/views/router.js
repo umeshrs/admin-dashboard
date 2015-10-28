0d85c8d35c7cd7d2073366bf6f1b60d54685eea3
Umesh <rsu1991@gmail.com>
2015-10-28 19:51:37 +0530

    Added login template and created a route for it.

--- client/views/router.js
+++ client/views/router.js
@@ -5,7 +5,7 @@ Router.configure({
 Router.route('/', function () {
   //ADD YOU ROUTES HERE
   //eg : this.render('home');
-  this.render('blankPage');
+  this.render('login');
 },{
  layoutTemplate:"defaultLayout" 
 });
