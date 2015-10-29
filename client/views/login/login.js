4e9b892d73fee72013b642ba9976919d16d21d51
Umesh <rsu1991@gmail.com>
2015-10-29 16:10:15 +0530

    Called getToken method from login template event handler to get the auth token for rocket chat.

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -6,9 +6,18 @@ Template.login.events({
     username = $("#login-username").val();
     password = $("#login-password").val();
     Meteor.loginWithPassword(username, password, function (error) {
-      console.log("error: ", error);
+      console.log("Error logging in: ", error);
       if (Meteor.userId()) {
         console.log("Login successful");
+        Meteor.call("getToken", username, password, function (error, result) {
+          console.log("result: ", result);
+          var domain, port, url;
+          domain = "192.168.1.122";
+          port = "4000";
+          url = "http://" + domain + ":" + port + "?token=" + result.authToken;
+          console.log(url);
+          Session.set("src", url);
+        });
         Router.go('/home');
       } else {
         console.log("Login unsuccessful. Please try again.");
