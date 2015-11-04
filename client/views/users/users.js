f5fd7f3d1d4f226ef7c1755a57db1ac437cea6a3
Umesh <rsu1991@gmail.com>
2015-11-04 17:11:18 +0530

    Defined event handlers to remove an user. Edited Meteor.users.allow callback to permit remove function if the user is an administrator.

--- client/views/users/users.js
+++ client/views/users/users.js
@@ -1,5 +1,24 @@
+Template.users.onRendered(function () {
+  Session.setDefault("currentUser", {});
+});
+
 Template.users.helpers({
   users: function () {
-    return Meteor.users.find();
+    return Meteor.users.find({}, { sort: { createdAt: 1 } });
+  }
+});
+
+Template.users.events({
+  'click #remove-user-btn': function () {
+    Session.set("currentUser", this);
+  },
+  'click #remove-user-modal-btn': function () {
+    Meteor.users.remove({ _id: Session.get("currentUser")._id }, function (error, result) {
+      if (error) {
+        console.log("Error removing document: ", error);
+      } else {
+        console.log("Number of documents removed: " + result);
+      }
+    })
   }
 });
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -11,7 +11,7 @@ Meteor.methods({
   getToken: function (username, password) {
     console.log("inside getToken method.");
     var domain, port, url, result, token;
-    domain = "192.168.1.8";
+    domain = "192.168.1.122";
     port = "4000";
     url = "http://" + domain + ":" + port + "/api/login";
     result = HTTP.post(url,
@@ -25,4 +25,20 @@ Meteor.methods({
     console.log("return value: ", { authToken: token });
     return { authToken: token };
   }
+});
+
+Meteor.users.allow({
+  remove: function (userId, doc) {
+    var currentUser, userRole;
+    currentUser = Meteor.users.findOne({ _id: userId }, { fields: { 'profile.role': 1 } });
+    userRole = currentUser.profile && currentUser.profile.role;
+    if (userId !== doc._id && userRole === "administrator") {
+      console.log("Access granted. You are an administrator and you are not trying to delete your own document.");
+      return true;
+    } else {
+      console.log("Access denied. You are not an administrator or you are trying to delete your own document.");
+      return false;
+    }
+  },
+  fetch: []
 });
