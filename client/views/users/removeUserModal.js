8acbc113173434705074af699d7e31893db59f01
Umesh <rsu1991@gmail.com>
2015-11-09 23:47:02 +0530

    Synchronized user deletion with rocket chat instance.

--- client/views/users/removeUserModal.js
+++ client/views/users/removeUserModal.js
@@ -1,5 +1,9 @@
+var rocketChatConnection;
+rocketChatConnection = DDP.connect('http://192.168.1.122:4000');
+
 Template.removeUserModal.events({
   'click #remove-user-modal-btn': function () {
+
     Meteor.users.remove({ _id: Session.get("currentUser")._id }, function (error, result) {
       if (error) {
         console.log("Error removing document: ", error);
@@ -14,6 +18,24 @@ Template.removeUserModal.events({
         $('body').pgNotification(options).show();
         console.log("Number of documents removed: " + result);
       }
-    })
+    });
+
+    // console.log("rocketChatConnection status: ", rocketChatConnection.status());
+
+    rocketChatConnection.call("getUserId", Session.get("currentUser").username, function (error, result) {
+      if (error) {
+        console.log("Error getting rocket chat userId: ", error);
+      } else {
+        rocketChatConnection.call("deleteUser", result, function (error, result) {
+          if (error) {
+            console.log("Error deleting user from rocket chat instance: ", error);
+          } else {
+            console.log(Session.get("currentUser").username + " successfully removed from rocket chat instance.");
+          }
+        });
+      }
+    });
+
+    // rocketChatConnection.disconnect();
   }
 });
