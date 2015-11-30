deb75e9cf92a8a8bb724d6c2119ef9ad91eb8b72
Umesh <rsu1991@gmail.com>
2015-11-30 11:48:27 +0530

    Defined surveys helper for manageSurveys template.

---
+++ client/views/surveys/manageSurveys.js
@@ -0,0 +1,14 @@
+Template.manageSurveys.helpers({
+  surveys: function () {
+    return [{
+      title: "Survey 1",
+      dateCreated: "27/11/2015"
+    }, {
+      title: "Survey 2",
+      dateCreated: "29/11/2015"
+    }, {
+      title: "Survey 3",
+      dateCreated: "30/11/2015"
+    }]
+  }
+});
