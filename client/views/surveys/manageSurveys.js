26ee4e0ff024e67675a80ca4939a45ab9416cb64
Umesh <rsu1991@gmail.com>
2015-12-03 12:15:01 +0530

    surveys helper returns surveys from collection.

--- client/views/surveys/manageSurveys.js
+++ client/views/surveys/manageSurveys.js
@@ -8,16 +8,7 @@ Template.manageSurveys.onRendered(function () {
 
 Template.manageSurveys.helpers({
   surveys: function () {
-    return [{
-      title: "Survey 1",
-      dateCreated: "27/11/2015"
-    }, {
-      title: "Survey 2",
-      dateCreated: "29/11/2015"
-    }, {
-      title: "Survey 3",
-      dateCreated: "30/11/2015"
-    }]
+    return Surveys.find();
   }
 });
 
