d458d110471fae613faf3be0756ffc023e6031f5
Umesh <rsu1991@gmail.com>
2015-12-03 13:42:43 +0530

    Removed old initialization code for switchery elements.

--- client/views/surveys/manageSurveys.js
+++ client/views/surveys/manageSurveys.js
@@ -1,11 +1,3 @@
-Template.manageSurveys.onRendered(function () {
-  var elems = Array.prototype.slice.call(document.querySelectorAll('.switchery'));
-  // Success color: #10CFBD
-  elems.forEach(function (html) {
-    var switchery = new Switchery(html, {color: '#10CFBD'});
-  });
-});
-
 Template.manageSurveys.helpers({
   surveys: function () {
     return Surveys.find({}, { fields: { title: 1, createdAt: 1, published: 1 } });
