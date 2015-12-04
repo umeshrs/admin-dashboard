f33d8f2007307ab6fc134454cc3eac793591be38
Umesh <rsu1991@gmail.com>
2015-12-04 20:00:05 +0530

    Defined method pushNotifications to notify members of new surveys.

---
+++ server/methods/pushNotifications.js
@@ -0,0 +1,18 @@
+Meteor.methods({
+  pushNotifications: function (surveyId) {
+    var i, users, notification, survey;
+    survey = Surveys.findOne(surveyId, { fields: { title: 1 } });
+    notification = {
+      surveyId: surveyId,
+      title: (survey && survey.title),
+      text: "Please take this survey.",
+      status: "unread",
+      type: "info",
+      createdAt: new Date()
+    };
+    users = Meteor.users.find({ 'profile.role': "member" }, { fields: { _id: 1 } }).fetch();
+    for (i = 0; i < users.length; i++) {
+      Meteor.users.update(users[i]._id, { $push: { 'profile.notifications': notification } });
+    }
+  }
+});
