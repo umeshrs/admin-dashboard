ad4ad074e284407c7607f927e04371935ccb69df
Umesh <rsu1991@gmail.com>
2015-12-05 18:05:08 +0530

    Defined event handler for submit survey button which calls the addResponse method with user's response as an argument.

--- client/views/surveys/viewSurvey.js
+++ client/views/surveys/viewSurvey.js
@@ -1,5 +1,17 @@
 Template.viewSurvey.events({
   'click #cancel-btn': function () {
     Router.go('/manage-surveys');
+  },
+  'click #submit-response-btn': function (event, template) {
+    var questions = template.$(".question-wrapper"), i, response = [];
+
+    for (i = 0; i < questions.length; i++) {
+      response.push({
+        question: this.questions[i].text,
+        response: template.$(questions[i]).find("input:radio:checked").val()
+      });
+    }
+
+    Meteor.call("addResponse", this._id, response);
   }
 });
