a953dce0ebfaf99ba56e11a1681b713d11cc9456
Umesh <rsu1991@gmail.com>
2015-12-07 16:09:42 +0530

    Added success and error notifications for removing survey.

--- client/views/surveys/manageSurveys.js
+++ client/views/surveys/manageSurveys.js
@@ -18,12 +18,22 @@ Template.manageSurveys.events({
     Router.go('/preview-survey/' + this._id);
   },
   'click .remove-survey-btn': function () {
+    var surveyTitle = this.title;
     Meteor.call("removeSurvey", this._id, function (error, result) {
+      var notificationOptions = {
+        style: "bar",
+        position: "top",
+        type: "success"
+      };
       if (error) {
         console.log("Error invoking method 'removeSurvey':", error.message);
+        notificationOptions.message = "<b>Error!</b> " + error.reason;
+        notificationOptions.type = "error";
       } else {
+        notificationOptions.message = "<b>Success!</b> " + surveyTitle + " has been removed.";
         console.log(result + " document(s) removed from Surveys collection.");
       }
+      $('body').pgNotification(notificationOptions).show();
     });
   }
 });
