0d75fd7d8d004b4591251834488fe2803bcf4541
Umesh <rsu1991@gmail.com>
2015-12-07 17:24:50 +0530

    Updated styles for form header and question titles.

--- client/views/surveys/addSurvey.js
+++ client/views/surveys/addSurvey.js
@@ -45,6 +45,12 @@ Template.addSurvey.events({
       }
       $('body').pgNotification(notificationOptions).show();
     });
+  },
+  'focusin .form-control': function (event, template) {
+    template.$(event.target).closest(".form-group-default").css({"padding-bottom": "9px", "border-bottom": "2px solid"});
+  },
+  'focusout .form-control': function (event, template) {
+    template.$(event.target).closest(".form-group-default").css({"padding-bottom": "10px", "border-bottom": "1px inset"});
   }
 });
 
