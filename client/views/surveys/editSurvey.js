044b51da87b70a7b68038f44900149b08410b78d
Umesh <rsu1991@gmail.com>
2015-12-08 22:15:18 +0530

    Bring focus to form title when editSurvey template is rendered.

--- client/views/surveys/editSurvey.js
+++ client/views/surveys/editSurvey.js
@@ -1,3 +1,7 @@
+Template.editSurvey.onRendered(function () {
+  this.$("#form-title").focus();
+});
+
 Template.editSurvey.events({
   'focusin .form-control': function (event, template) {
     template.$(event.target).closest(".form-group-default").css({"padding-bottom": "9px", "border-bottom": "2px solid"});
