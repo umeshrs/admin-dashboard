ff9e4c5e935fcde397aa64b8125c98314ff05fa8
Umesh <rsu1991@gmail.com>
2015-12-08 21:59:52 +0530

    Defined event handler for add question button in editSurvey template.

--- client/views/surveys/editSurvey.js
+++ client/views/surveys/editSurvey.js
@@ -4,5 +4,9 @@ Template.editSurvey.events({
   },
   'focusout .form-control': function (event, template) {
     template.$(event.target).closest(".form-group-default").css({"padding-bottom": "10px", "border-bottom": "1px inset"});
+  },
+  'click #add-question-btn': function (event) {
+    var parentNode = $(event.target).closest(".btns-wrapper").siblings(".questions-wrapper")[0];
+    Blaze.render(Template.question, parentNode);
   }
 });
