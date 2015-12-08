7592ca1d7c29081b1efa9217944590ead6406bb6
Umesh <rsu1991@gmail.com>
2015-12-08 16:13:28 +0530

    Close notification dropdown when clicked anywhere on the page except the notification area.

--- client/views/layout.js
+++ client/views/layout.js
@@ -1,3 +1,11 @@
+Template.defaultLayout.events({
+  'click': function (event, template) {
+    if (! template.$(event.target).closest(".dropdown").length) {
+      Session.set("showNotificationDropdown", false);
+    }
+  }
+});
+
 Template.defaultLayout.rendered = function (){
 	//INIT PAGES : API CALLS
 	$('[data-pages="sidebar"]').sidebar();
