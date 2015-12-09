91fc1209d2d3a837c5a529a87606f6bce40b2d74
Umesh <rsu1991@gmail.com>
2015-12-09 13:08:40 +0530

    Defined event handler for save survey btn in editSurvey template.

--- client/views/surveys/editSurvey.js
+++ client/views/surveys/editSurvey.js
@@ -12,5 +12,45 @@ Template.editSurvey.events({
   'click #add-question-btn': function (event) {
     var parentNode = $(event.target).closest(".btns-wrapper").siblings(".questions-wrapper")[0];
     Blaze.render(Template.question, parentNode);
+  },
+  'click #save-survey-btn': function (event, template) {
+    var survey = {}, questions, question, options, option, i, j;
+    survey.title = template.$("#form-title").val();
+    survey.description = template.$("#form-description").val();
+    survey.questions = [];
+    questions = template.$(".question-wrapper");
+
+    for (i = 0; i < questions.length; i++) {
+      question = {};
+      question.text = template.$(questions[i]).find(".question").val();
+      question.options = [];
+      options = template.$(questions[i]).find(".option");
+
+      for (j = 0; j < options.length; j++) {
+        option = {};
+        option.text = template.$(options[j]).val();
+        question.options.push(option);
+      }
+
+      survey.questions.push(question);
+    }
+
+    Meteor.call("updateSurvey", this._id, survey, function (error, result) {
+      var notificationOptions = {
+        style: "bar",
+        position: "top",
+        type: "success"
+      };
+      if (error) {
+        console.log("Error invoking method 'updateSurvey':", error.message);
+        notificationOptions.message = "<b>Error!</b> " + error.reason;
+        notificationOptions.type = "error";
+      } else {
+        Router.go('/manage-surveys');
+        console.log(result + " document(s) updated in Surveys collection.");
+        notificationOptions.message = "<b>Success!</b> Changes made to the survey have been saved.";
+      }
+      $('body').pgNotification(notificationOptions).show();
+    });
   }
 });
