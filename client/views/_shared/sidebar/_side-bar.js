5b9b32bdf4db0f4f52a2a34d67519e7fd9dfbb3d
Umesh <rsu1991@gmail.com>
2015-12-22 17:35:17 +0530

    Used pathFor helper for sidebar links. Checked if route is defined before performing split on its path.

--- client/views/_shared/sidebar/_side-bar.js
+++ client/views/_shared/sidebar/_side-bar.js
@@ -1,4 +1,4 @@
 Template.sideBar.onRendered(function () {
-  var menuItem = Router.current().route._path.split("/", 2)[1];
+  var menuItem = Router.current().route && Router.current().route._path.split("/", 2)[1];
   $(".sidebar-menu ul.menu-items a[href*='" + menuItem + "']").siblings(".icon-thumbnail").addClass("bg-primary");
 });
