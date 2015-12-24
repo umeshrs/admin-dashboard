dd18468861dd84b55c3c6296f7e3fbcde6bfcade
Umesh <rsu1991@gmail.com>
2015-12-24 14:17:46 +0530

    Initialised stores list scrollbar. Added scrollbar styles.

--- client/views/maps/storesList.js
+++ client/views/maps/storesList.js
@@ -1,8 +1,10 @@
 Template.storesList.onRendered(function () {
-  $("#stores-list").height($(window).innerHeight() - 60 - 56 - 68 - $("#stores-list-container h1").outerHeight(true));
+  this.$("#stores-list").height($(window).innerHeight() - 60 - 56 - 68 - this.$("h1").outerHeight(true));
   $(window).resize(function () {
     $("#stores-list").height($(window).innerHeight() - 60 - 56 - 68 - $("#stores-list-container h1").outerHeight(true));
   });
+
+  this.$('[class|="scrollbar"]').scrollbar();
 });
 
 Template.storesList.helpers({
