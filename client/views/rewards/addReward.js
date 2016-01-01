06d074479ae4082c86f11c811752222b72b1b145
Umesh <rsu1991@gmail.com>
2016-01-01 16:57:48 +0530

    Defined event handler for save reward button in addReward template.

--- client/views/rewards/addReward.js
+++ client/views/rewards/addReward.js
@@ -5,3 +5,45 @@ Template.addReward.onRendered(function () {
     todayBtn: 'linked',
   });
 });
+
+Template.addReward.events({
+  'click #save-reward-btn': function (event, template) {
+    let reward = {
+      title: template.$("#reward-title").val(),
+      description: template.$("#reward-description").val(),
+      points: template.$("#reward-points").val(),
+      count: template.$("#reward-count").val(),
+      validTill: template.$(".date").datepicker('getDate'),
+      published: template.$(".switchery")[0].checked,
+      createdAt: new Date()
+    };
+
+    Meteor.call('insertReward', reward, function (error, result) {
+      let notificationOptions = {
+        style: "bar",
+        position: "top",
+        type: "error"
+      };
+      if (error) {
+        console.log(`Error invoking method 'insertReward'. Error: ${error.message}`);
+        switch (error.error) {
+          case "not-logged-in":
+            notificationOptions.message = "<b>Oops!</b> You must be logged in to add a reward.";
+            break;
+          case "not-authorized":
+            notificationOptions.message = "<b>Oops!</b> You are not authorized to add a reward.";
+            break;
+          default:
+            notificationOptions.message = "<b>Oops!</b> Something went wrong while adding the new reward. Please try again.";
+            break;
+        }
+      } else {
+        Router.go('/rewards');
+        console.log(`New reward inserted into rewards collection. Result: ${result}`);
+        notificationOptions.message = "<b>Success!</b> New reward added.";
+        notificationOptions.type = "success";
+      }
+      $('body').pgNotification(notificationOptions).show();
+    });
+  }
+});
