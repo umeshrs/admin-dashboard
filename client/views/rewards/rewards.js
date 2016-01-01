19b83705e2faf10ecee08b83153f9ddea7078dae
Umesh <rsu1991@gmail.com>
2016-01-01 17:19:22 +0530

    Defined rewards helper.

--- client/views/rewards/rewards.js
+++ client/views/rewards/rewards.js
@@ -1,3 +1,9 @@
+Template.rewards.helpers({
+  rewards() {
+    return Rewards.find({}, { sort: { createdAt: 1} });
+  }
+});
+
 Template.rewards.events({
   'click #add-reward-btn': function () {
     Router.go('/rewards/add-reward');
