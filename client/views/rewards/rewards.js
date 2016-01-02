d75638754b5ed8d4e8a5874f93b59f45e70b33b1
Umesh <rsu1991@gmail.com>
2016-01-02 13:28:15 +0530

    Added tooltips for action buttons in rewards template.

--- client/views/rewards/rewards.js
+++ client/views/rewards/rewards.js
@@ -1,3 +1,12 @@
+Template.rewards.onRendered(function () {
+  Tracker.autorun(function () {
+    if (Rewards.find({}, { sort: { createdAt: 1} }).count() > 0) {
+      $('[data-toggle="tooltip"]').tooltip({ container: 'body' });
+      $('[data-tooltip-toggle="tooltip"]').tooltip({ container: 'body' });
+    }
+  });
+});
+
 Template.rewards.helpers({
   rewards() {
     return Rewards.find({}, { sort: { createdAt: 1} });
