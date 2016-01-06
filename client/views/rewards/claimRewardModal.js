72bf528e52bf18ca0bf99831e532e45c42316fc4
Umesh <rsu1991@gmail.com>
2016-01-06 14:57:55 +0530

    Enabled scrolling if modal content height is greater than window height.

--- client/views/rewards/claimRewardModal.js
+++ client/views/rewards/claimRewardModal.js
@@ -1,3 +1,17 @@
+Template.claimRewardModal.onRendered(function () {
+  // remove class 'disable-scroll' from modal if its content height is greater than the window height
+  $("#claim-reward-modal").on('shown.bs.modal', function (event) {
+    if ($(event.target).find(".modal-content").innerHeight() > $(window).innerHeight()) {
+      $(event.target).removeClass('disable-scroll');
+    }
+  });
+
+  // add class 'disable-scroll' (default) to modal when a modal is closed
+  $("#claim-reward-modal").on('hidden.bs.modal', function (event) {
+    $(event.target).addClass('disable-scroll');
+  });
+});
+
 Template.claimRewardModal.helpers({
   reward() {
     return Session.get("currentReward");
