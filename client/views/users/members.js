b44405624b5d6cd1d8ac46af11a6ac829ac18501
Umesh <rsu1991@gmail.com>
2016-01-07 15:20:42 +0530

    Renamed users.js to members.js. Updated file contents.

--- client/views/users/members.js
+++ client/views/users/members.js
@@ -1,19 +1,19 @@
 Meteor.subscribe("usersData");
 
-Template.users.onRendered(function () {
+Template.members.onRendered(function () {
   Session.setDefault("currentUser", {});
   $('[data-toggle="tooltip"]').tooltip({ container: 'body' });
   $('[data-tooltip-toggle="tooltip"]').tooltip({ container: 'body' });
 });
 
-Template.users.helpers({
-  users: function () {
-    return Meteor.users.find({}, { sort: { createdAt: 1 } });
+Template.members.helpers({
+  members: function () {
+    return Meteor.users.find({ 'profile.role': "member" }, { sort: { createdAt: 1 } });
   }
 });
 
-Template.users.events({
-  'click #remove-user-btn': function () {
+Template.members.events({
+  'click .remove-member-btn': function () {
     Session.set("currentUser", this);
   }
 });
