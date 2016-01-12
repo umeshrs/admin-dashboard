2f3503339619a4237d3be84e99b68731cc39818a
Umesh <rsu1991@gmail.com>
2016-01-12 16:22:49 +0530

    Updated remove member modal button event handler.

--- client/views/members/removeMemberModal.js
+++ client/views/members/removeMemberModal.js
@@ -1,56 +1,34 @@
-var rocketChatConnection, wekanConnection;
-rocketChatConnection = DDP.connect("http://" + ROCKET_CHAT_DOMAIN + ":" + ROCKET_CHAT_PORT);
-wekanConnection = DDP.connect("http://" + WEKAN_DOMAIN + ":" + WEKAN_PORT);
-
 Template.removeMemberModal.events({
   'click #remove-member-modal-btn': function () {
+    let user = Session.get("currentUser");
 
-    Meteor.users.remove({ _id: Session.get("currentUser")._id }, function (error, result) {
+    Meteor.call("removeUser", user._id, user.username, function (error, result) {
+      let notificationOptions = {
+        style: "bar",
+        position: "top",
+        type: "error"
+      };
+
       if (error) {
-        console.log("Error removing document: ", error);
-      } else {
-        var options;
-        options = {
-          style: "bar",
-          position: "top",
-          message: Session.get("currentUser").profile.name + " has been removed from the database.",
-          type: "info"
+        console.log(`Error invoking method 'removeUser'. Error: ${error.message}.`);
+        switch (error.error) {
+          case "not-logged-in":
+            notificationOptions.message = "<b>Oops!</b> You must be logged in to remove a member.";
+            break;
+          case "not-authorized":
+            notificationOptions.message = "<b>Oops!</b> You are not authorized to remove a member.";
+            break;
+          default:
+            notificationOptions.message = "<b>Oops!</b> Something went wrong while removing the member. Please try again.";
+            break;
         }
-        $('body').pgNotification(options).show();
-        console.log(Session.get("currentUser").username + " removed from admin app instance. ");
-      }
-    });
-
-    // console.log("rocketChatConnection status: ", rocketChatConnection.status());
-
-    rocketChatConnection.call("getUserId", Session.get("currentUser").username, function (error, result) {
-      if (error) {
-        console.log("Error getting rocket chat userId: ", error);
       } else {
-        rocketChatConnection.call("deleteUser", result, function (error, result) {
-          if (error) {
-            console.log("Error deleting user from rocket chat instance: ", error);
-          } else {
-            console.log(Session.get("currentUser").username + " removed from rocket chat instance.");
-          }
-        });
+        console.log(`${result} document(s) removed from users collection. User removed: '${user.username}'.`);
+        notificationOptions.message = `<b>Success!</b> ${user.profile && user.profile.name} has been removed.`;
+        notificationOptions.type = "success";
       }
-    });
 
-    // console.log("wekanConnection status: ", wekanConnection.status());
-
-    wekanConnection.call("getUserId", Session.get("currentUser").username, function (error, result) {
-      if (error) {
-        console.log("Error getting wekan userId: ", error.reason);
-      } else {
-        wekanConnection.call("deleteUser", result, function (error, result) {
-          if (error) {
-            console.log("Error deleting user from wekan instance: ", error);
-          } else {
-            console.log(Session.get("currentUser").username + " removed from wekan instance.");
-          }
-        });
-      }
+      $('body').pgNotification(notificationOptions).show();
     });
   }
 });
