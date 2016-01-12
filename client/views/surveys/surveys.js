a0d6d7c73a29aa559350edf150449262fa7086f4
Umesh <rsu1991@gmail.com>
2016-01-12 12:58:17 +0530

    Added tooltips for view survey buttons in surveys template.

--- client/views/surveys/surveys.js
+++ client/views/surveys/surveys.js
@@ -1,3 +1,11 @@
+Template.surveys.onRendered(function () {
+  Tracker.autorun(function () {
+    if (Surveys.find({}, { fields: { _id: 1 }, sort: { createdAt: 1} }).count() > 0) {
+      $('[data-toggle="tooltip"]').tooltip({ container: 'body' });
+    }
+  });
+});
+
 Template.surveys.helpers({
   surveys: function () {
     return Surveys.find({}, {
@@ -9,6 +17,7 @@ Template.surveys.helpers({
 
 Template.surveys.events({
   'click .view-survey-btn': function () {
+    $('[data-toggle="tooltip"]').tooltip('hide');
     Router.go('/surveys/view-survey/' + this._id);
   }
 });
