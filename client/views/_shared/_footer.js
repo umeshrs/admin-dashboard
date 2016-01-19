d6ff9f9c640639ea068f19f197d3664268324087
Umesh <rsu1991@gmail.com>
2016-01-19 11:58:34 +0530

    Keep site language choice over page reloads.

--- client/views/_shared/_footer.js
+++ client/views/_shared/_footer.js
@@ -1,6 +1,11 @@
 Template.footer.onRendered(function () {
   // set default language of site based on the selected attribute of select#language element
-  TAPi18n.setLanguage(this.$("#language").val());
+  if (! localStorage.getItem("language")) {
+    TAPi18n.setLanguage(this.$("#language").val());
+    localStorage.setItem("language", this.$("#language").val());
+  } else {
+    this.$("#language").val(localStorage.getItem("language"));
+  }
 
   $('[data-init-plugin="select2"]').select2({
       minimumResultsForSearch: ($(this).attr('data-disable-search') == 'true' ? -1 : 1)
@@ -14,5 +19,6 @@ Template.footer.onRendered(function () {
 Template.footer.events({
   'change #language': function (event, template) {
     TAPi18n.setLanguage(template.$(event.target).val());
+    localStorage.setItem("language", template.$(event.target).val());
   }
 });
