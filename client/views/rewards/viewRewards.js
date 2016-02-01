711aa9bfce96c820f5e5317501bffcf0fcce7811
Umesh <rsu1991@gmail.com>
2016-02-01 19:23:46 +0530

    Updated viewRewards template's rewards helper. Sorted rewards in ascending order of points needed.

--- client/views/rewards/viewRewards.js
+++ client/views/rewards/viewRewards.js
@@ -11,15 +11,7 @@ Template.viewRewards.helpers({
     return (Meteor.user() && Meteor.user().profile && Meteor.user().profile.rewardPoints) || 0;
   },
   rewards() {
-    return Rewards.find({ published: true, availableCount: { $gt: 0 } }, {
-      fields: {
-        title: 1,
-        description: 1,
-        validTill: 1,
-        points: 1,
-        createdAt: 1
-      },
-      sort: { createdAt: 1} });
+    return Rewards.find({}, { sort: { points: 1 } });
   }
 });
 
