64c265446202bb5a40f265ee66867aa06269a298
Umesh <rsu1991@gmail.com>
2016-02-02 17:54:42 +0530

    Updated survey publication.

--- server/publications/survey.js
+++ server/publications/survey.js
@@ -2,9 +2,7 @@ Meteor.publish("survey", function (surveyId) {
   if (this.userId) {
     let currentUser = Meteor.users.findOne(this.userId, { fields: { 'profile.role': 1 } });
     if (currentUser.profile && currentUser.profile.role === "administrator") {
-      return Surveys.find(surveyId, {
-        fields: { responses: 0 }
-      });
+      return Surveys.find(surveyId);
     } else {
       return Surveys.find(surveyId, {
         fields: { responses: 0, published: 0 }
