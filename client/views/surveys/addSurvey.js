357e33c2d65f00dcd69ca0263ed75c97e562e089
Umesh <rsu1991@gmail.com>
2016-02-03 12:31:37 +0530

    Updated addSurvey event handlers.

--- client/views/surveys/addSurvey.js
+++ client/views/surveys/addSurvey.js
@@ -10,48 +10,62 @@ Template.addSurvey.onRendered(function () {
 });
 
 Template.addSurvey.events({
-  'click #add-question-btn': function (event) {
-    var parentNode = $(event.target).closest(".btns-wrapper").siblings(".questions-wrapper")[0];
+  'click #add-question-btn': function (event, template) {
+    let parentNode = template.$(event.target).closest(".btns-wrapper").siblings(".questions-wrapper")[0];
     Blaze.render(Template.question, parentNode);
   },
-  'click #save-survey-btn': function (event) {
-    var survey = {}, questions, question, options, option, i, j;
-    survey.title = $(event.target).closest("#add-survey-form").find("#form-title").val();
-    survey.description = $(event.target).closest("#add-survey-form").find("#form-description").val();
+  'click #save-survey-btn': function (event, template) {
+    let survey = {};
+    survey.title = template.$(event.target).closest("#add-survey-form").find("#form-title").val();
+    survey.description = template.$(event.target).closest("#add-survey-form").find("#form-description").val();
     survey.questions = [];
-    questions = $(event.target).closest("#add-survey-form").find(".question-wrapper");
+    let questions = template.$(event.target).closest("#add-survey-form").find(".question-wrapper");
 
-    for (i = 0; i < questions.length; i++) {
-      question = {};
-      question.text = $(questions[i]).find(".question").val();
+    for (let i = 0; i < questions.length; i++) {
+      let question = {};
+      question.text = template.$(questions[i]).find(".question").val();
       question.options = [];
-      options = $(questions[i]).find(".option");
+      let options = template.$(questions[i]).find(".option");
 
-      for (j = 0; j < options.length; j++) {
-        option = {};
-        option.text = $(options[j]).val();
+      for (let j = 0; j < options.length; j++) {
+        let option = {};
+        option.text = template.$(options[j]).val();
         question.options.push(option);
       }
 
       survey.questions.push(question);
     }
 
-    survey.published = true;
+    survey.published = false;
+    survey.publishDate = template.$("#publish-date").closest(".date").datepicker('getDate');
+    survey.expiryDate = template.$("#expiry-date").closest(".date").datepicker('getDate');
     survey.createdAt = new Date();
 
-    Surveys.insert(survey, function (error, result) {
-      var notificationOptions = {
+    Meteor.call("insertSurvey", survey, function (error, result) {
+      let notificationOptions = {
         style: "bar",
         position: "top",
-        type: "success"
+        type: "error"
       };
       if (error) {
-        console.log("Error creating survey. Error: ", error.message);
-        notificationOptions.message = "<b>Error!</b> Something went wrong while creating the new survey. Please try again.";
-        notificationOptions.type = "error";
+        console.log(`Error invoking method 'insertSurvey'. Error: ${error.message}`);
+        switch (error.error) {
+          case "not-logged-in":
+            notificationOptions.message = "<b>Oops!</b> You must be logged in to add a survey.";
+            break;
+          case "not-authorized":
+            notificationOptions.message = "<b>Oops!</b> You are not authorized to add a survey.";
+            break;
+          default:
+            notificationOptions.message = "<b>Oops!</b> Something went wrong while adding the new survey. Please try again.";
+            break;
+        }
       } else {
         Router.go('/surveys');
-        notificationOptions.message = "<b>Success!</b> New survey created.";
+        console.log(`New survey inserted into surveys collection. Result: ${result}`);
+        notificationOptions.message = "<b>Success!</b> New survey added.";
+        notificationOptions.type = "success";
+
         Meteor.call("pushNotifications", result);
       }
       $('body').pgNotification(notificationOptions).show();
