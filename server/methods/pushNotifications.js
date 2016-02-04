036707d21fa241477143380eab05c6b824c5055f
Umesh <rsu1991@gmail.com>
2016-02-04 13:12:31 +0530

    Updated method pushNotifications.

--- server/methods/pushNotifications.js
+++ server/methods/pushNotifications.js
@@ -1,18 +1,7 @@
 Meteor.methods({
-  pushNotifications: function (surveyId) {
-    var i, users, notification, survey;
-    survey = Surveys.findOne(surveyId, { fields: { title: 1 } });
-    notification = {
-      surveyId: surveyId,
-      title: (survey && survey.title),
-      text: "Please take this survey.",
-      isUnread: true,
-      type: "info",
-      createdAt: new Date()
-    };
-    users = Meteor.users.find({ 'profile.role': "member" }, { fields: { _id: 1 } }).fetch();
-    for (i = 0; i < users.length; i++) {
-      Meteor.users.update(users[i]._id, { $push: { 'profile.notifications': notification } });
-    }
+  pushNotifications: function (notification) {
+    Meteor.users.update({ 'profile.role': "member" },
+      { $push: { 'profile.notifications': notification } },
+      { multi: true });
   }
 });
