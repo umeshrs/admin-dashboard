25cfd0d068e90b2a85951d02d0b1b481243eb1c3
Umesh <rsu1991@gmail.com>
2016-02-08 12:23:06 +0530

    Updated method pushNotifications.

--- server/methods/pushNotifications.js
+++ server/methods/pushNotifications.js
@@ -1,5 +1,6 @@
 Meteor.methods({
   pushNotifications: function (notification) {
+    console.log(`Pushing notification for ${notification.title}...`);
     Meteor.users.update({ 'profile.role': "member" },
       { $push: { 'profile.notifications': notification } },
       { multi: true });
