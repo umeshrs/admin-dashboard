aee3986476f70b53210632d179c5b19b30186190
Umesh <rsu1991@gmail.com>
2016-02-09 15:06:44 +0530

    Wrote cronjob to remove expired tasks at midnight everyday.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -11,5 +11,17 @@ Meteor.startup(function () {
     }
   });
 
+  SyncedCron.add({
+    name: 'Remove expired tasks',
+    schedule: function (parser) {
+      // parser is a later.parse object
+      return parser.cron('0 0 * * *');
+    },
+    job: function () {
+      console.log(`[Cronjob -> Remove expired tasks]Removing tasks with expiry date before ${new Date(new Date().setHours(0, 0, 0, 0)).toLocaleString()}...`);
+      Meteor.call("removeExpiredTasks");
+    }
+  });
+
   SyncedCron.start();
 });
