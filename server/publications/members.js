8d87570bc47015cdcf241b397b0b06fa4c27abfe
Umesh <rsu1991@gmail.com>
2016-02-15 13:52:11 +0530

    Updated members publication.

--- server/publications/members.js
+++ server/publications/members.js
@@ -1,8 +1,11 @@
-Meteor.publish("members", function () {
+Meteor.publish("members", function (skip, limit) {
   if (this.userId) {
     let currentUser = Meteor.users.findOne(this.userId, { fields: { 'profile.role': 1 } });
     if (currentUser.profile && currentUser.profile.role === "administrator") {
       return Meteor.users.find({ 'profile.role': "member" }, {
+        sort: { createdAt: 1 },
+        skip: skip,
+        limit: limit,
         fields: { services: 0 }
       });
     } else {
