409b8667f7412dcf997088964dc112be536d27e3
Umesh <rsu1991@gmail.com>
2016-02-15 21:01:37 +0530

    Updated rewards publication.

--- server/publications/rewards.js
+++ server/publications/rewards.js
@@ -1,13 +1,20 @@
-Meteor.publish("rewards", function () {
+Meteor.publish("rewards", function (skip, limit) {
   if (this.userId) {
     let currentUser = Meteor.users.findOne(this.userId);
     if (currentUser.profile && currentUser.profile.role === "administrator") {
-      return Rewards.find();
+      return Rewards.find({}, {
+        sort: { createdAt: 1 },
+        skip: skip,
+        limit: limit
+      });
     } else {
       return Rewards.find({
         published: true,
         availableCount: { $gt: 0 }
       }, {
+        sort: { points: 1 },
+        skip: skip,
+        limit: limit,
         fields: { availableCount: 0, claimCount: 0, published: 0, createdAt: 0 }
       });
     }
