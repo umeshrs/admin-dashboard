2563b4e5fe0bd2db6b11b7fdb4e4dba1f6d5c4fa
Umesh <rsu1991@gmail.com>
2016-02-16 17:43:24 +0530

    Updated surveys publication.

--- server/publications/surveys.js
+++ server/publications/surveys.js
@@ -1,13 +1,23 @@
-Meteor.publish("surveys", function () {
+Meteor.publish("surveys", function (skip, limit) {
   if (this.userId) {
     let currentUser = Meteor.users.findOne(this.userId, { fields: { 'profile.role': 1 } });
     if (currentUser.profile && currentUser.profile.role === "administrator") {
       return Surveys.find({}, {
+        sort: { createdAt: 1 },
+        skip: skip,
+        limit: limit,
         fields: { description: 0, questions: 0 }
       });
     } else {
       let currentDate = new Date();
-      return Surveys.find({ published: true, publishDate: { $lte: currentDate }, expiryDate: { $gte: currentDate } }, {
+      return Surveys.find({
+        published: true,
+        publishDate: { $lte: currentDate },
+        expiryDate: { $gte: currentDate }
+      }, {
+        sort: { publishDate: 1 },
+        skip: skip,
+        limit: limit,
         fields: { title: 1, publishDate: 1, expiryDate: 1 }
       });
     }
