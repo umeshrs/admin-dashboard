2e8b53a35c70e0e3eee90b7c5e8e3016ce0daf0a
Umesh <rsu1991@gmail.com>
2016-02-21 05:25:32 +0530

    Stored switchery in a reactive variable after it is rendered. Defined event handler to keep data context up to date with switchery checked state.

--- client/views/switchery/switchery.js
+++ client/views/switchery/switchery.js
@@ -1,5 +1,15 @@
 Template.switchery.onRendered(function () {
-  let checkbox = this.find('.switchery'), switchery;
-  checkbox.checked = Template.currentData() && Template.currentData().checked ? true : false;
-  switchery = new Switchery(checkbox, { color: '#67A023', size: 'medium' });
+  let template = this, checkbox = template.find('.switchery');
+  template.data.checked = template.data.checked ? true : false;
+  checkbox.checked = template.data.checked;
+  template.switchery = new ReactiveVar( new Switchery(checkbox, { color: '#67A023', size: 'medium' }) );
+  template.autorun(function () {
+    Template.parentData().switchery = template.switchery.get();
+  });
+});
+
+Template.switchery.events({
+  'change .switchery': function (event, template) {
+    Template.currentData().checked = template.switchery.get().isChecked();
+  }
 });
