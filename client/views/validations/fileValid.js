43bb924ec9e6f00ed7b445beb7205f09d3bdfe18
Umesh <rsu1991@gmail.com>
2016-02-24 15:09:45 +0530

    Updated validation method fileValid to accept CSV files submitted from a Windows system.

--- client/views/validations/fileValid.js
+++ client/views/validations/fileValid.js
@@ -1,5 +1,6 @@
 $.validator.addMethod("fileValid", function (value, element) {
   let file = (element.type === "file") && (element.files.length > 0) && element.files[0];
-  let valid = file && file.type === "text/csv";
+  // "application/vnd.ms-excel" is the file type of a CSV file when it is uploaded from a Windows system
+  let valid = file && (file.type === "text/csv" || file.type === "application/vnd.ms-excel");
   return valid ? true : false;
 }, "Invalid file type. Only CSV file is accepted.");
